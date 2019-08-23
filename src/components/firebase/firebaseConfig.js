a8cdc21170e99dc26e8dcf7494a6352f298e96b7
Steven <safwebdev@gmail.com>
2019-08-23 17:17:27 +0800

    remove firebase config

--- src/components/firebase/firebaseConfig.js
+++
@@ -1,9 +0,0 @@
-export default{
-    apiKey: "AIzaSyDLhC99i9xf0_7NrjFa6nP5ME6DaYaa6XQ",
-    authDomain: "vuewfs-prod-5a7b7.firebaseapp.com",
-    databaseURL: "https://vuewfs-prod-5a7b7.firebaseio.com",
-    projectId: "vuewfs-prod-5a7b7",
-    storageBucket: "vuewfs-prod-5a7b7.appspot.com",
-    messagingSenderId: "8017948035",
-    appId: "1:8017948035:web:6caed2e08e159799"
-}
